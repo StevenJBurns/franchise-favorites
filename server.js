433a06b8f42f729a132353b1f45e716ff786d87d
StevenJBurns <stevenjburns@msn.com>
2018-11-11 18:09:20 -0700

    Installed Mongoose and Morgan via NPM; basic test connection added to server.js

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require("path");
 const chalk = require("chalk");
 const express = require("express");
 const favicon = require("serve-favicon");
+const mongoose = require("mongoose");
+const logger = require("morgan");
 
 /* Local Dependencies */
 let router = require("./router.js");
@@ -15,6 +17,11 @@ require("dotenv").config();
 /* Clear the console and create the Express server */
 const server = new express();
 console.clear();
+
+/* connect to MongoDB via mongoose */
+mongoose.connect('mongodb://localhost:27017/franchise-favorites', { useNewUrlParser: true }, () => {
+  console.log(chalk.bgWhite.black(`            Connected to DB            `))
+});
 
 /* serve-favicon Middleware */
 server.use(favicon(path.join(__dirname, "public", "favicon.ico")));

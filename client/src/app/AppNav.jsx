cd71ca0d48b6c278e67eac833428ffa0ed10eda2
StevenJBurns <stevenjburns@msn.com>
2018-11-11 21:16:34 -0700

    Added dummy items to AppNav ul and set the ul to display flex row

--- client/src/app/AppNav.jsx
+++ client/src/app/AppNav.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 
-const AppNav = () => {
+const AppNav = (props) => {
   return (
-    <nav id="app-nav"></nav>
+    <nav id="app-nav">
+      <ul>
+        <li>Franchises</li>
+        <li>Episodes</li>
+        <li>Characters</li>
+      </ul>
+    </nav>
   );
 };
 

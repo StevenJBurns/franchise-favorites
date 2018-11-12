d96fbfda3e899d8d0b0700eae5e465ccaeaaacac
StevenJBurns <stevenjburns@msn.com>
2018-11-12 04:44:12 -0700

    Added Font Awesome icons to AppHeader and changed layout to flexbox row

--- client/src/components/app/AppHeader.jsx
+++ client/src/components/app/AppHeader.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
+/* Font Awesome! */
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilm, faStar } from '@fortawesome/free-solid-svg-icons';
+
 
 const AppHeader = (props) => {
   return (
     <header id="app-header">
+      <FontAwesomeIcon icon={ faFilm } size="3x" />
+      <FontAwesomeIcon icon={ faStar } size="1x" spin />
       <h2>FRANCHISE FAVORITES</h2>
     </header>
   );

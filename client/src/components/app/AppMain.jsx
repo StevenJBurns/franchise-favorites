fe7db3357ead4e765739efd69e6c07ce80619bb1
StevenJBurns <stevenjburns@msn.com>
2018-11-12 00:10:11 -0700

    Refactored AppMain and AppNav to use React DOM Router using pages

--- client/src/components/app/AppMain.jsx
+++ client/src/components/app/AppMain.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { Switch, Route, withRouter } from "react-router-dom";
+
+import PageHome from "../pages/PageHome";
+import PageMyFavorites from "../pages/PageMyFavorites";
+import PageFranchises from "../pages/PageFranchises";
+import Page404 from "../pages/Page404";
 
 
 const AppMain = () => {
   return (
-    <main id="app-main">
-
-    </main>
+    <React.Fragment>
+      <Switch>
+        <Route exact path="/" component={PageHome} />
+        <Route exact path="/my-favorites" component={PageMyFavorites} />
+        <Route exact path="/franchises" component={PageFranchises} />
+        <Route component={Page404} />
+      </Switch>
+    </React.Fragment>
   );
 };
 
-export default AppMain;
+export default withRouter(AppMain);
--- client/src/components/app/AppNav.jsx
+++ client/src/components/app/AppNav.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { NavLink } from 'react-router-dom';
 
 
 const AppNav = (props) => {
   return (
     <nav id="app-nav">
       <ul>
-        <li>Franchises</li>
-        <li>Episodes</li>
-        <li>Characters</li>
+        <li><NavLink to="/" exact>HOME</NavLink></li>
+        <li><NavLink to="/my-favorites">MY FAVORITES</NavLink></li>
+        <li><NavLink to="/franchises">FRANCHISES</NavLink></li>
       </ul>
     </nav>
   );

ce1ca2e8e4889785ce1de050896e01cd9db1ed01
StevenJBurns <stevenjburns@msn.com>
2018-11-13 09:43:17 -0700

    Data access works from top to bottom now; mLab stores data in cloud MongoDB -- Express API endpoints call for data from mLab -- the React client PageFranchises calls fetch to Express API and creates tiles for franchises

--- client/src/components/pages/PageFranchises.jsx
+++ client/src/components/pages/PageFranchises.jsx
@@ -3,28 +3,31 @@ import React from "react";
 import FranchiseTile from "../ui/FranchiseTile.jsx";
 
 
-const testData = [
-  { title: "Marvel", imageURL: "" },
-  { title: "Star Wars", imageURL: "" },
-  { title: "Star Trek", imageURL: "" },
-  { title: "Harry Potter",imageURL: "" },
-  { title: "Mission Impossible", imageURL: "" },
-  { title: "Fast & Furious", imageURL: "" },
-  { title: "Aliens", imageURL: "" },
-  { title: "Die Hard", imageURL: "" },
-  { title: "Jurassic", imageURL: "" },
-  { title: "Hunger Games", imageURL: "" },
-  { title: "Indiana Jones", imageURL: "" }
-];
+class PageFranchises extends React.Component {
+  constructor(props) {
+    super(props);
 
-const PageFranchises = () => {
-  return (
-    <main>
-      <section id="section-franchise-tiles">
-        { testData.map((item, index) => (<FranchiseTile key={index} title={item.title} imageURL={item.imageURL} />)) }
-      </section>
-    </main>
-  );
+    this.state = {
+      franchises: []
+    };
+  }
+
+
+  componentDidMount() {
+    fetch("/api/franchises")
+      .then(data => data.json())
+      .then(json => this.setState({franchises: json}));
+  }
+
+  render() {
+    return (
+      <main>
+        <section id="section-franchise-tiles">
+          { this.state.franchises.map((item, index) => (<FranchiseTile key={index} title={item.title} imageURL={item.imageURL} />)) }
+        </section>
+      </main>
+    );
+  };
 };
 
 export default PageFranchises;

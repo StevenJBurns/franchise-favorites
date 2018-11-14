4994c845fdee5da20eb0349e2641d8eabf427124
StevenJBurns <stevenjburns@msn.com>
2018-11-13 18:23:44 -0700

    on PageSignUp and PageSignIn started structure with HTML forms for login and register as well as Brooke inspired styles

--- client/src/components/pages/PageSignIn.jsx
+++ client/src/components/pages/PageSignIn.jsx
@@ -3,7 +3,16 @@ import React from "react";
 const PageSignIn = () => {
   return (
     <main>
-      <h1>Sign In</h1>
+      <form id="form-sign-in">
+        <label for="input-email">email</label>
+        <input type="email" id="input-email"/>
+        <label for="input-password">password</label>
+        <input type="password" id="input-password"/>
+        <button type="submit">SUBMIT</button>
+        <hr></hr>
+        <button type="submit">FACEBOOK</button>
+        <button type="submit">TWITTER</button>
+      </form>
     </main>
   );
 };
--- client/src/components/pages/PageSignUp.jsx
+++ client/src/components/pages/PageSignUp.jsx
@@ -3,7 +3,17 @@ import React from "react";
 const PageSignUp = () => {
   return (
     <main>
-      <h1>Sign Up</h1>
+      <form id="form-sign-up">
+        <label for="input-first-name">First Name</label>
+        <input type="text" id="input-first-name"/>
+        <label for="input-last-name">Last Name</label>
+        <input type="text" id="input-last-name"/>
+        <label for="input-email">email</label>
+        <input type="email" id="input-email"/>
+        <label for="input-password">password</label>
+        <input type="password" id="input-password"/>
+        <button type="submit">SUBMIT</button>
+      </form>
     </main>
   );
 };

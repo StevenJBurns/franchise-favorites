defeafc945b6ef7932195a60c663cfad12c2a33a
StevenJBurns <stevenjburns@msn.com>
2018-11-15 00:48:43 -0700

    Even More CSS refactoring -- for AppFooter

--- client/src/components/app/AppFooter.jsx
+++ client/src/components/app/AppFooter.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
+import "./AppFooter.css";
+
 
 const AppFooter = () => {
   const year = new Date().getFullYear();
+
   return (
     <footer id="app-footer">
       <h4>&copy; {year} &bull; Steven J Burns</h4>

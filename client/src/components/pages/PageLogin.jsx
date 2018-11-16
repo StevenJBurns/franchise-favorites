3eca9e1e3f1647f1b8dccec2b60856e18bb2b865
StevenJBurns <stevenjburns@msn.com>
2018-11-16 00:41:13 -0700

    Cleaned up for --> htmlFor in JSX labels on PageLogin

--- client/src/components/pages/PageLogin.jsx
+++ client/src/components/pages/PageLogin.jsx
@@ -7,9 +7,9 @@ const PageSignIn = () => {
   return (
     <main>
       <form id="form-sign-in">
-        <label for="input-email">email</label>
+        <label htmlFor="input-email">email</label>
         <input type="email" id="input-email"/>
-        <label for="input-password">password</label>
+        <label htmlFor="input-password">password</label>
         <input type="password" id="input-password"/>
         <button type="submit">SUBMIT</button>
         <hr></hr>

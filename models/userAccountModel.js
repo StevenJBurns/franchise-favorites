b122b2df8ff6a0b11c323ede8b763abb226ff265
StevenJBurns <stevenjburns@msn.com>
2018-11-21 22:35:38 -0700

    Formatting changes to userAccountModel.js

--- models/userAccountModel.js
+++ models/userAccountModel.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
-const bcryptjs = require("bcryptjs");
-
 
 const userAccountSchema = new mongoose.Schema({
   userEmail: { type: String, required: true },
   password: { type: String, required: true },
   favorites: { type: Array}
-}, { collection: 'userAccounts' });
+},
+{
+  collection: 'userAccounts'
+});
 
-
-module.exports = userAccountModel = mongoose.model("userAccounts", userAccountSchema);
+module.exports = UserAccount = mongoose.model("userAccounts", userAccountSchema);

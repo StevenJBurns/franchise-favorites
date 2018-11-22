3a59b535809deb824221b370bacc97119fe88fd8
StevenJBurns <stevenjburns@msn.com>
2018-11-22 05:28:11 -0700

    Reorganization of /register final 201 response and savedUser

--- routers/routerAuth.js
+++ routers/routerAuth.js
@@ -12,10 +12,9 @@ routerAuth.post("/register", async (req, res, next) => {
     
   if (!email || !password) throw new Error("email and password are required");
 
+  /* Check for existing email in mLab; Send 400 error back if it already exists */
   let exisitngUser = await UserAccount.findOne({ "userEmail": email });
-
-  /* Send 400 error back if the email already exists on mLab */
-  if (exisitngUser) return res.status(400).send("That user already exists");
+  if (exisitngUser) return res.status(400).json({"message": "That user already exists"});
 
   /* hash the password and create a new user with req.body.email and the hashed password */
   let hashedPassword = await bcryptjs.hash(password, SALT); 
@@ -25,47 +24,23 @@ routerAuth.post("/register", async (req, res, next) => {
     "password": hashedPassword
   });
 
-  /* assuming hashing finished correctly, save newUser to mLab via Mongoose */ 
-  let savedUser = await newUser.save();
-  
-  /* Fire off a 201 response to the client with the savedUser object minus the password property */
-  delete savedUser["password"];
-  res.status(201).json(savedUser);
+  /* Save newUser to mLab via Mongoose and 201 with new userEmail */ 
+  let { userEmail } = await newUser.save();
+  res.status(201).json({"message": `created new user: ${userEmail}`});  
 });
 
 routerAuth.post("/login", async (req, res, next) => {
   const { email, password } = req.body;
 
-  let user;
+  let exisitingUser = await userAccountModel.findOne({ userEmail: email });
+    
+  if (!exisitingUser) return res.status(400).json({ "error": "email not found"});
 
-  try {
-    user = await userAccountModel.findOne({ userEmail: email }, (err, u) => {
-      /* Send 500 error back if Mongo/Mongoose don't play nicely */
-      if (err) res.status(500).send("500: Server Error");
-      
-      if (!u) throw new Error("400: email not found");
-    });
-  } catch (err) {
-    // console.error(err);
-    return res.status(400).send("email not found");
-  }
-
-  try {
-    bcryptjs.compare(password, user["password"], (err, match) => {
-      if (err) return res.status(500).send();
-
-      console.log("match: ", match);
-      
-      if (match) {
-        res.status(200).json({ "200: user found": user["userEmail"] });
-      } else {
-        throw new Error("400: bad password");
-      }
-    });
-  } catch (err) {
-    // console.error(err);
-    return res.status(400).send("400: bad password");
-  }
+  let passwordsMatch = await bcryptjs.compare(password, user["password"]);
+  
+  return passwordsMatch ?
+    res.status(200).json({ "message": `user found": ${user["userEmail"]}` }) :
+    res.status(400).json({ "message": "bad password" });
 });
 
 module.exports = routerAuth;

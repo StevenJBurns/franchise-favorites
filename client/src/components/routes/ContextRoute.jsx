667378a8935f5e4f7518a26ba80aa4ae48b6c3cc
StevenJBurns <stevenjburns@msn.com>
2018-11-23 18:38:56 -0700

    Changes to ProtectedRoute and ContextRoute components;  ProtectedRoute wraps generic React Router Route component inside CurrentUserContext; ContextRoute will probably be unnecessary in the future

--- client/src/components/routes/ContextRoute.jsx
+++ client/src/components/routes/ContextRoute.jsx
@@ -1,13 +1,17 @@
+/* React and React Route DOM imports */
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route } from "react-router-dom";
 
-import isAuthenticated from "./isAuthenticated.js";
+/* Import the user context component to be used as consumer */ 
+import { CurrentUserContext } from "../app/App.jsx";
 
 
 const ContextRoute = ({ component: Component, context: Context, ...rest}) => (
-  <Route { ...rest } render={ props => (
-    isAuthenticated() ? <Component {...props} /> : <Redirect to="/login" />
-  )} />
+  <CurrentUserContext.Consumer>
+    { ({ register, login}) => 
+      (<Route { ...rest } render={ props => (<Component {...props} />)} />)
+    }
+  </CurrentUserContext.Consumer>
 );
 
 export default ContextRoute;
--- client/src/components/routes/ProtectedRoute.jsx
+++ client/src/components/routes/ProtectedRoute.jsx
@@ -1,13 +1,19 @@
+/* React and React Route DOM imports */
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
-import isAuthenticated from "./isAuthenticated.js";
+/* Import the user context component to be used as consumer */ 
+import { CurrentUserContext } from "../app/App.jsx";
 
 
 const ProtectedRoute = ({ component: Component, ...rest}) => (
-  <Route { ...rest } render={ props => (
-    isAuthenticated() ? <Component {...props} /> : <Redirect to="/login" />
-  )} />
+  <CurrentUserContext.Consumer>
+    { ({state}) => (
+      <Route { ...rest } render={ props => (
+        state.isAuthenticated ? <Component {...props} /> : <Redirect to="/login" />
+      )} />)
+    }
+  </CurrentUserContext.Consumer>
 );
 
 export default ProtectedRoute;

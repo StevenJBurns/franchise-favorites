7b2e34b4cee12539044185b4bc9df6360b944a58
StevenJBurns <stevenjburns@msn.com>
2018-11-24 13:04:06 -0700

    Created boilerplate PageFranchiseDetails jsx and css files

---
+++ client/src/components/pages/PageFranchiseDetail.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./PageFranchiseDetail.css";
+
+
+class PageFranchiseDetail extends React.Component {
+  constructor() {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <h2>FRANCHISE DETAIL</h2>
+    );
+  };
+};
+
+export default PageFranchiseDetail;

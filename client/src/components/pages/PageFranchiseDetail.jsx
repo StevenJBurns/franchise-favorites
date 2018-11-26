ad4b90f3a82f9f0b3d36399cfa3b615eee36f6d3
StevenJBurns <stevenjburns@msn.com>
2018-11-26 02:51:24 -0700

    Added styling to movie lists on PageFranchiseDetail -- needs more clean up

--- client/src/components/pages/PageFranchiseDetail.jsx
+++ client/src/components/pages/PageFranchiseDetail.jsx
@@ -6,16 +6,19 @@ import "./PageFranchiseDetail.css";
 
 
 const PageFranchiseDetail = (props) => {
-  const { franchise } = props;
-  console.log(franchise);
-
   return (
     <AppContext.Consumer>
       {
-      ({ franchises}) => (
+      ({ user, franchises}) => (
         franchises.selected && 
           <main>
+            { !user.isAuthenticated && <h2 id="h2-not-authorized">YOU NEED TO LOG IN</h2> }
             <h2>{franchises.selected.title}</h2>
+            <ul id="ul-franchise-movies">
+              {
+                franchises.selected.movies.map((movie, index) => (<li key={index}>{movie}</li>))
+              }
+            </ul>
           </main>
         )
       }

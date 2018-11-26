17be21e77293515432fdbf3b51451fa8e7863055
StevenJBurns <stevenjburns@msn.com>
2018-11-25 18:41:35 -0700

    Added some really bad content to PageHome; created PageHome.css; stole movie reel PNG from the interwebz

--- client/src/components/pages/PageHome.jsx
+++ client/src/components/pages/PageHome.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
+import "./PageHome.css";
+
 
 const PageHome = () => {
   return (
-    <main>
+    <main id="main-home">
       <h1>PAGE HOME</h1>
+      <p className="p-home">Welcome to Franchise-Favorites, a simple little app where you can rank the movies of a grouped franchise</p>
+      <p className="p-home">To create your ranked lists you need to be logged in.  Create an account if you havent already</p>
     </main>
   );
 };

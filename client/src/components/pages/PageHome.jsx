44fb3ffc1e16b6dbdadae4a58c5ed80d938e29cb
StevenJBurns <stevenjburns@msn.com>
2018-11-29 06:06:03 -0700

    Minor content change on PageHome

--- client/src/components/pages/PageHome.jsx
+++ client/src/components/pages/PageHome.jsx
@@ -7,7 +7,7 @@ import "./PageHome.css";
 const PageHome = () => {
   return (
     <main id="main-home">
-      <p className="p-home">Welcome to <strong>Franchise &#9734; Favorites</strong>, a simple little app where you can rank order your favorite movies within a franchise or even the franchises themselves.</p>
+      <p className="p-home"><strong>Franchise &#9733; Favorites</strong> is a simple little app where you can rank order your favorite movies within a franchise or even the franchises themselves.</p>
       <p className="p-home">To get started making your own lists you will need to <Link to="/register">register</Link> and <Link to="/login">log in</Link>.</p>
     </main>
   );

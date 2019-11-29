cf09675b3a453a4c8c13394fd42566f30aaac7ad
StevenJBurns <stevenjburns@msn.com>
2019-11-29 14:46:29 -0700

    chore: chore: temp rendering fix for PageFranchiseList

--- client/src/components/pages/PageFranchiseList.jsx
+++ client/src/components/pages/PageFranchiseList.jsx
@@ -1,37 +1,38 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
+import { Link, useParams } from "react-router-dom";
 import { UserContext } from "../app/App.jsx";
 import FranchiseTile from "../ui/FranchiseTile.jsx";
-
 import "./PageFranchiseList.css";
 
 
 const PageFranchiseList = (props) => {  
-  const { match } = props;
+  const { match } = useParams();
 
-  return (
-    <UserContext.Consumer>
-      {
-      ({ franchises, changeFranchise }) => (
-        <main id="main-franchises">
-          <h2>Select a franchise below to start...</h2>
-          <ul id="ul-franchise-tiles">
-          {
-            franchises.list.map((franchise, index) => (
-              <li key={index}>
-                <FranchiseTile {...props} franchise={franchise} changeFranchise={changeFranchise}>
-                  <Link to={`${match.url}/${franchise.slug}`} />
-                </FranchiseTile>
-              </li>)
-            )
-          }
-          </ul>
-        </main>)
-      }
-    </UserContext.Consumer>
+  console.log('match: ', match);
 
-  );
+  return (<h2>PageFranchiseList</h2>);
+
+  // return (
+  //   <UserContext.Consumer>
+  //     {
+  //     ({ franchises, changeFranchise }) => (
+  //       <main id="main-franchises">
+  //         <h2>Select a franchise below to start...</h2>
+  //         <ul id="ul-franchise-tiles">
+  //         {
+  //           franchises.list.map((franchise, index) => (
+  //             <li key={index}>
+  //               <FranchiseTile {...props} franchise={franchise} changeFranchise={changeFranchise}>
+  //                 <Link to={`${match.url}/${franchise.slug}`} />
+  //               </FranchiseTile>
+  //             </li>)
+  //           )
+  //         }
+  //         </ul>
+  //       </main>)
+  //     }
+  //   </UserContext.Consumer>
+  // );
 };
 
 export default PageFranchiseList;

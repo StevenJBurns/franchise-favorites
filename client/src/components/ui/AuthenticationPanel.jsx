f0a55b83d3c8a11602c4e4c0cb6d0bc784b3de44
StevenJBurns <stevenjburns@msn.com>
2019-11-29 13:54:22 -0700

    fix: correct context object used in Authentication Panel

--- client/src/components/ui/AuthenticationPanel.jsx
+++ client/src/components/ui/AuthenticationPanel.jsx
@@ -6,29 +6,33 @@ import "./Authentication.css";
 
 
 export const AuthenticationPanel = props => {
-  const user = React.UserContext(UserContext);
-  
-  return (
-    <UserContext.Consumer>
-      {        
-      ({ user, logout }) => {
-        let status = user.isAuthenticated ? <h6 id="h6-logged-in">{user.userEmail}</h6> : <h6 id="h6-not-logged-in">NOT LOGGED IN</h6>
-        let visible = user.isAuthenticated ? {display: "block"} : {display: "none"};
-        let hidden = user.isAuthenticated ? {display: "none"} : {display: "block"};
+  const user = React.useContext(UserContext);
+
+  console.log(user);
+
+  return null
+
+  // return (
+  //   <UserContext.Consumer>
+  //     {        
+  //     ({ user, logout }) => {
+  //       let status = user.isAuthenticated ? <h6 id="h6-logged-in">{user.userEmail}</h6> : <h6 id="h6-not-logged-in">NOT LOGGED IN</h6>
+  //       let visible = user.isAuthenticated ? {display: "block"} : {display: "none"};
+  //       let hidden = user.isAuthenticated ? {display: "none"} : {display: "block"};
       
-        return (
-          <section>
-            <ul>
-              <li style={hidden}><NavLink to="/login" exact>LOGIN</NavLink></li>
-              <li style={hidden}><NavLink to="/register" exact>REGISTER</NavLink></li>
-              <li style={visible} onClick={logout}><NavLink to="/" exact>LOGOUT</NavLink></li>
-            </ul>
-            { status }
-          </section>
-        )}
-      }
-    </UserContext.Consumer>
-  );
+  //       return (
+  //         <section>
+  //           <ul>
+  //             <li style={hidden}><NavLink to="/login" exact>LOGIN</NavLink></li>
+  //             <li style={hidden}><NavLink to="/register" exact>REGISTER</NavLink></li>
+  //             <li style={visible} onClick={logout}><NavLink to="/" exact>LOGOUT</NavLink></li>
+  //           </ul>
+  //           { status }
+  //         </section>
+  //       )}
+  //     }
+  //   </UserContext.Consumer>
+  // );
 };
 
 export default AuthenticationPanel;
